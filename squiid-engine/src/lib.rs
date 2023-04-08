afc3719cfb5651302a4c2f2741588a3eb70e077a
TabulateJarl8 <tabulatejarl8@gmail.com>
2023-04-07 23:29:56 -0400

    consolidate commands into a hashmap for implementation of sending available commands to client

--- squiid-engine/src/lib.rs
+++ squiid-engine/src/lib.rs
@@ -2,9 +2,72 @@ mod bucket;
 mod engine;
 mod utils;
 
+use std::{collections::HashMap, borrow::BorrowMut};
+
 use engine::Engine;
 
 use crate::bucket::Bucket;
+
+macro_rules! function_map_entry {
+    ($function_map:expr,$name:expr,$func_name:ident) => {
+        $function_map.insert(
+            String::from($name),
+            Box::new(|engine: &mut Engine| engine.borrow_mut().$func_name()) as Box<EngineFunction>
+        )
+    };
+}
+
+type EngineFunction = dyn Fn(&mut Engine) -> Result<(), &'static str>;
+
+fn create_function_map() -> HashMap<String, Box<EngineFunction>> {
+    let mut function_map = HashMap::new();
+
+    // Insert string keys and function objects into the hashmap
+
+    function_map_entry!(function_map, "add", add);
+    function_map_entry!(function_map, "subtract", subtract);
+    function_map_entry!(function_map, "divide", divide);
+    function_map_entry!(function_map, "multiply", multiply);
+    function_map_entry!(function_map, "power", power);
+    function_map_entry!(function_map, "sqrt", sqrt);
+    function_map_entry!(function_map, "mod", modulo);
+    function_map_entry!(function_map, "sin", sin);
+    function_map_entry!(function_map, "cos", cos);
+    function_map_entry!(function_map, "tan", tan);
+    function_map_entry!(function_map, "sec", sec);
+    function_map_entry!(function_map, "csc", csc);
+    function_map_entry!(function_map, "cot", cot);
+    function_map_entry!(function_map, "asin", asin);
+    function_map_entry!(function_map, "acos", acos);
+    function_map_entry!(function_map, "atan", atan);
+    function_map_entry!(function_map, "log", log);
+    function_map_entry!(function_map, "logb", logb);
+    function_map_entry!(function_map, "ln", ln);
+    function_map_entry!(function_map, "abs", abs);
+    function_map_entry!(function_map, "eq", eq);
+    function_map_entry!(function_map, "gt", gt);
+    function_map_entry!(function_map, "lt", lt);
+    function_map_entry!(function_map, "gte", gte);
+    function_map_entry!(function_map, "lte", lte);
+    function_map_entry!(function_map, "round", round);
+    function_map_entry!(function_map, "invert", invert);
+    function_map_entry!(function_map, "drop", drop);
+    function_map_entry!(function_map, "swap", swap);
+    function_map_entry!(function_map, "dup", dup);
+    function_map_entry!(function_map, "rolldown", roll_down);
+    function_map_entry!(function_map, "rollup", roll_up);
+    function_map_entry!(function_map, "store", store);
+    function_map_entry!(function_map, "purge", purge);
+    function_map_entry!(function_map, "invstore", invstore);
+    function_map_entry!(function_map, "clear", clear);
+    function_map_entry!(function_map, "clear", clear);
+    function_map_entry!(function_map, "undo", undo);
+
+    // manually insert refresh since it doesn't use an engine method
+    function_map.insert(String::from("refresh"), Box::new(|_engine: &mut Engine| Ok(())));
+
+    function_map
+}
 
 pub fn start_server(address: Option<&str>) {
     // create zeromq socket to listen on
@@ -43,50 +106,19 @@ pub fn start_server(address: Option<&str>) {
             engine.history.push_back(engine.stack.clone());
         }
 
+        // create hashmap of available commands
+        let commands = create_function_map();
+
         let result = match recieved {
-            "add" => engine.add(),
-            "subtract" => engine.subtract(),
-            "multiply" => engine.multiply(),
-            "divide" => engine.divide(),
-            "power" => engine.power(),
-            "sqrt" => engine.sqrt(),
-            "mod" => engine.modulo(),
-            "sin" => engine.sin(),
-            "cos" => engine.cos(),
-            "tan" => engine.tan(),
-            "sec" => engine.sec(),
-            "csc" => engine.csc(),
-            "cot" => engine.cot(),
-            "asin" => engine.asin(),
-            "acos" => engine.acos(),
-            "atan" => engine.atan(),
-            "log" => engine.log(),
-            "logb" => engine.logb(),
-            "ln" => engine.ln(),
-            "abs" => engine.abs(),
-            "eq" => engine.eq(),
-            "gt" => engine.gt(),
-            "lt" => engine.lt(),
-            "gte" => engine.gte(),
-            "lte" => engine.lte(),
-            "round" => engine.round(),
-            "invert" => engine.invert(),
-            "drop" => engine.drop(),
-            "swap" => engine.swap(),
-            "dup" => engine.dup(),
-            "rolldown" => engine.roll_down(),
-            "rollup" => engine.roll_up(),
-            "store" => engine.store(),
-            "purge" => engine.purge(),
-            "invstore" => engine.invstore(),
-            "clear" => engine.clear(),
-            "refresh" => Ok(()),
-            "undo" => engine.undo(),
             "quit" => break,
-            recieved => {
-                let _ = engine.add_item_to_stack(Bucket::from(recieved.to_string()));
-                Ok(())
-            }
+            recieved => 
+                match commands.get(recieved) {
+                    Some(func) => func(engine.borrow_mut()),
+                    None => {
+                        let _ = engine.add_item_to_stack(Bucket::from(recieved.to_string()));
+                        Ok(())
+                    }
+                }
         };
 
         let mut formatted_response = String::new();

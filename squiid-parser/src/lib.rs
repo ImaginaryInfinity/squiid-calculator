191fd7d21a5b494424da860a8468079d7d5eef8f
TabulateJarl8 <tabulatejarl8@gmail.com>
2023-04-16 19:50:28 -0400

    mostly working parser implementation

--- squiid-parser/src/lib.rs
+++ squiid-parser/src/lib.rs
@@ -1,9 +1,10 @@
 use crate::lex::lex;
-use tokens::Token;
+use parser::shunting_yard_parser;
 
 mod lex;
 mod tokens;
+mod parser;
 
-pub fn parse(input: &str) -> Vec<Token> {
-    lex(input).unwrap()
+pub fn parse(input: &str) -> Vec<&str> {
+    shunting_yard_parser(lex(input).unwrap())
 }
---
+++ squiid-parser/src/parser.rs
@@ -0,0 +1,77 @@
+use std::collections::HashMap;
+
+use crate::tokens::Token;
+
+pub fn shunting_yard_parser<'a>(tokens: Vec<Token<'a>>) -> Vec<&'a str> {
+    let mut output_queue: Vec<&'a str> = Vec::new();
+    let mut operator_stack: Vec<&'a str> = Vec::new();
+    let precedence_map = HashMap::from([
+        ("^", 4),
+        ("*", 3),
+        ("/", 3),
+        ("%", 3),
+        ("+", 2),
+        ("-", 2),
+        ("(", 1),
+    ]);
+
+    let function_mappings = HashMap::from([
+        ("+", "add"),
+        ("-", "subtract"),
+        ("/", "divide"),
+        ("*", "multiply"),
+        ("%", "mod"),
+        ("^", "power"),
+        ("=", "invstore"),
+    ]);
+
+    for token in tokens {
+        match token {
+            Token::Function(token_name) | Token::LParen(token_name) => {
+                operator_stack.push(token_name);
+            }
+            Token::VariableAssign(token_name) | Token::VariableRecal(token_name) | Token::Constant(token_name) | Token::ScientificNotation(token_name) | Token::Float(token_name) | Token::Int(token_name) | Token::PrevAns(token_name) => {
+                output_queue.push(token_name);
+            }
+            Token::Comma(_) => {
+                while let Some(operator) = operator_stack.pop() {
+                    if operator == "(" {
+                        operator_stack.push(operator);
+                        break;
+                    } else {
+                        output_queue.push(operator);
+                    }
+                }
+            }
+            Token::RParen(_) => {
+                while let Some(operator) = operator_stack.pop() {
+                    if operator == "(" {
+                        break;
+                    } else {
+                        output_queue.push(operator);
+                    }
+                }
+            }
+            Token::Equal(token_name) | Token::Power(token_name) | Token::Multiply(token_name) | Token::Divide(token_name) | Token::Modulo(token_name) | Token::Add(token_name) | Token::Subtract(token_name) => {
+                while let Some(operator) = operator_stack.pop() {
+                    if operator == "(" || precedence_map.get(operator) < precedence_map.get(token_name) {
+                        operator_stack.push(operator);
+                        break;
+                    } else {
+                        output_queue.push(operator);
+                    }
+                }
+                operator_stack.push(token_name);
+            }
+            Token::Negative(_) => {
+                output_queue.push("chs");
+            }
+        }
+    }
+
+    while let Some(operator) = operator_stack.pop() {
+        output_queue.push(operator);
+    }
+
+    output_queue
+}

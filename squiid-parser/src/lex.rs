b5e1bae4da11e5292020144c153c331c54096b8d
TabulateJarl8 <tabulatejarl8@gmail.com>
2023-04-16 03:23:27 -0400

    split off different parsing methods into seperate functions

--- squiid-parser/src/lex.rs
+++ squiid-parser/src/lex.rs
@@ -24,13 +24,104 @@ const RIGHT_SIDE_IMPLICIT: [Token; 9] = [
     Negative("_"),
 ];
 
+fn parse_subtract_sign(tokens: &mut Vec<Token>) {
+    // parse whether this is a negative sign or a minus operator
+    // it is a negative sign if:
+    //
+    // at the beginning of an expression
+    // at the beginning of an opening parenthesis (-3+6)
+    // after another operator (3+-5, 3*-5, 3^-5)
+    // as an argument in a function, so after a comma (function(3, -3))
+
+    let mut negative_replacements: Vec<usize> = Vec::new();
+
+    for (index, token) in tokens.iter().enumerate() {
+        if *token == Subtract("-") {
+            // at the beginning of an expression
+            if index == 0 {
+                negative_replacements.push(index);
+            } else {
+                // get the token before the current negative sign
+                match tokens[index - 1] {
+                    // at the beginning of an opening parenthesis (-3+6)
+                    Token::RParen("(") |
+                    // after another operator (3+-5, 3*-5, 3^-5)
+                    Token::Add("+") | Token::Subtract("-") | Token::Modulo("%") | Token::Multiply("*") | Token::Divide("/") | Token::Power("^") | Token::Equal("=") |
+                    // as an argument in a function, so after a comma (function(3, -3))
+                    Token::Comma(",") => {
+                        negative_replacements.push(index);
+                    },
+                    _ => (),
+                }
+            }
+        }
+    }
+
+    // do replacements
+    for index in negative_replacements {
+        tokens[index] = Negative("-");
+    }
+}
+
+fn parse_implicit_multiplication(tokens: &mut Vec<Token>) {
+    // Left side (current token):
+    // Function, VariableRecal, Constant, ScientificNotation, Float, Int, PrevAns, RParen
+    //
+    // Right Side (peek token):
+    // Function, VariableRecal, Constant, ScientificNotation, Float, Int, PrevAns, LParen, Negative
+    //
+    // Implicit multiplication happens if something on the left side list is followed by something on the right side list
+
+    let mut multiply_insertions = Vec::new();
+
+    for (index, token) in tokens.iter().enumerate() {
+        if LEFT_SIDE_IMPLICIT.contains(&token) {
+            // make sure that we aren't on the last token
+            if index + 1 != tokens.len() {
+                // there is a next token that is not an error, test if in right side multiplication table
+                if RIGHT_SIDE_IMPLICIT.contains(&tokens[index + 1]) {
+                    // implicit multiplication is needed at the index in front of the current token
+                    multiply_insertions.push(index + 1);
+                }
+            };
+        }
+    }
+
+    // do insertions
+    for (index, insertion_index) in multiply_insertions.iter().enumerate() {
+        // offset by index to take account for tokens vec growing in size
+        tokens.insert(*insertion_index + index, Multiply("*"));
+    }
+}
+
+fn parse_function_calls(tokens: &mut Vec<Token>) {
+    // split function tokens into the identifier and the opening parenthesis
+
+    let mut lparen_insertions = Vec::new();
+
+    for (index, token) in tokens.iter_mut().enumerate() {
+        match *token {
+            Function(name) => {
+                // mark need for LParen insertion
+                lparen_insertions.push(index + 1);
+                // trim off the trailing LParen
+                *token = Function(name.trim_end_matches('('));
+            }
+            _ => (),
+        }
+    }
+
+    // do insertions
+    for (index, insertion_index) in lparen_insertions.iter().enumerate() {
+        tokens.insert(*insertion_index + index, LParen("("));
+    }
+}
+
 // lex a given input string
 pub fn lex(input: &str) -> Result<Vec<Token>, String> {
-    let mut lex = Token::lexer(input).spanned().peekable();
+    let mut lex = Token::lexer(input).spanned();
     let mut tokens = Vec::new();
-    let mut index = 0;
 
-    // for (token, range) in lex {
     while let Some((token, range)) = lex.next() {
         if token.is_err() {
             return Err(format!(
@@ -39,73 +130,12 @@ pub fn lex(input: &str) -> Result<Vec<Token>, String> {
             ));
         }
 
-        let cur_token = token.unwrap();
-
-        match cur_token {
-            Token::Subtract(_) => {
-                // parse whether this is a negative sign or a minus operator
-                // it is a negative sign if:
-                //
-                // at the beginning of an expression
-                // at the beginning of an opening parenthesis (-3+6)
-                // after another operator (3+-5, 3*-5, 3^-5)
-                // as an argument in a function, so after a comma (function(3, -3))
-
-                // at the beginning of an expression
-                if index == 0 {
-                    tokens.push(Token::Negative("-"));
-                } else {
-                    match *tokens.last().unwrap() {
-                        // at the beginning of an opening parenthesis (-3+6)
-                        Token::RParen("(") |
-                        // after another operator (3+-5, 3*-5, 3^-5)
-                        Token::Add("+") | Token::Subtract("-") | Token::Modulo("%") | Token::Multiply("*") | Token::Divide("/") | Token::Power("^") | Token::Equal("=") |
-                        // as an argument in a function, so after a comma (function(3, -3))
-                        Token::Comma(",") => {
-                            tokens.push(Token::Negative("-"));
-                        },
-                        _ => tokens.push(Token::Subtract("-")),
-                    }
-                }
-            }
-            // Left side (current token):
-            // Function, VariableRecal, Constant, ScientificNotation, Float, Int, PrevAns, RParen
-            //
-            // Right Side (peek token):
-            // Function, VariableRecal, Constant, ScientificNotation, Float, Int, PrevAns, LParen, Negative
-            //
-            // Implicit multiplication happens if something on the left side list is followed by something on the right side list
-            token if LEFT_SIDE_IMPLICIT.contains(&token) => {
-                // push current token to tokens list
-                tokens.push(token);
-
-                match lex.peek() {
-                    Some(token) => match &token.0 {
-                        Ok(value) => {
-                            // there is a next token that is not an error, test if in right side multiplication table
-                            if RIGHT_SIDE_IMPLICIT.contains(value) {
-                                // implicit multiplication is needed
-                                tokens.push(Multiply("*"));
-                            }
-                        }
-                        Err(_) => {
-                            return Err(format!(
-                                "Unexpected token: {:?}",
-                                &input[token.1.start..token.1.end]
-                            ))
-                        }
-                    },
-                    None => {
-                        // no next token, do nothing
-                    }
-                };
-            }
-            item => tokens.push(item),
-        }
-
-        // manual tracking of index since enumerate() moves lex
-        index += 1;
+        tokens.push(token.unwrap());
     }
+
+    parse_subtract_sign(&mut tokens);
+    parse_implicit_multiplication(&mut tokens);
+    parse_function_calls(&mut tokens);
 
     Ok(tokens)
 }

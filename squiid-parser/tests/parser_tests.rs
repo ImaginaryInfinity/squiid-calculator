110075287265277ef23f8f9afd4e46c41d155bdf
TabulateJarl8 <tabulatejarl8@gmail.com>
2023-04-18 00:03:52 -0400

    finish integrating parser tests
    Closes #30

--- squiid-parser/tests/parser_tests.rs
+++ squiid-parser/tests/parser_tests.rs
@@ -191,15 +191,79 @@ fn test_shunting_yard_parser() {
     parse_and_compare("5 + 3 * 2", vec!["5", "3", "2", "*", "+"]);
     parse_and_compare("(5 + 3) * 2", vec!["5", "3", "+", "2", "*"]);
     parse_and_compare("sin(3 * 2) + 4", vec!["3", "2", "*", "sin", "4", "+"]);
-    parse_and_compare("5 * 3 + 2 * 4 / 6", vec!["5", "3", "*", "2", "4", "*", "6", "/", "+"]);
+    parse_and_compare(
+        "5 * 3 + 2 * 4 / 6",
+        vec!["5", "3", "*", "2", "4", "*", "6", "/", "+"],
+    );
     parse_and_compare("5 + 3 * sin(2)", vec!["5", "3", "2", "sin", "*", "+"]);
-    parse_and_compare("sqrt(4 + 9) * log(100)", vec!["4", "9", "+", "sqrt", "100", "log", "*"]);
-    parse_and_compare("3 ^ 2 - 4 * sqrt(16)", vec!["3", "2", "^", "4", "16", "sqrt", "*", "-"]);
-    parse_and_compare("sin(cos(tan(1))) + logb(2, 8)", vec!["1", "tan", "cos", "sin", "2", "8", "logb", "+"]);
-    parse_and_compare("(2 + 3) * 4 - abs(-5)", vec!["2", "3", "+", "4", "*", "5", "chs", "abs", "-"]);
-    parse_and_compare("sqrt(5 + 3 * sin(2)) / logb(2, 8)", vec!["5", "3", "2", "sin", "*", "+", "sqrt", "2", "8", "logb", "/"]);
-    parse_and_compare("2 ^ (3 + abs(-4)) * max(5, min(6, 7))", vec!["2", "3", "4", "chs", "abs", "+", "^", "5", "6", "7", "min", "max", "*"]);
-    parse_and_compare("sin(cos(2 * #PI / 3)) + 1 / (log(10) - 3.5)", vec!["2", "#PI", "*", "3", "/", "cos", "sin", "1", "10", "log", "3.5", "-", "/", "+"]);
-    parse_and_compare("3 * (4 + 5) / (6 - abs(-7) * (8 + 9))", vec!["3", "4", "5", "+", "*", "6", "7", "chs", "abs", "8", "9", "+", "*", "-", "/"]);
-    parse_and_compare("abs(-1 + 2) * ceil(sqrt(16)) - round(3.14159)", vec!["1", "chs", "2", "+", "abs", "16", "sqrt", "ceil", "*", "3.14159", "round", "-"]);
+    parse_and_compare(
+        "sqrt(4 + 9) * log(100)",
+        vec!["4", "9", "+", "sqrt", "100", "log", "*"],
+    );
+    parse_and_compare(
+        "3 ^ 2 - 4 * sqrt(16)",
+        vec!["3", "2", "^", "4", "16", "sqrt", "*", "-"],
+    );
+    parse_and_compare(
+        "sin(cos(tan(1))) + logb(2, 8)",
+        vec!["1", "tan", "cos", "sin", "2", "8", "logb", "+"],
+    );
+    parse_and_compare(
+        "(2 + 3) * 4 - abs(-5)",
+        vec!["2", "3", "+", "4", "*", "5", "chs", "abs", "-"],
+    );
+    parse_and_compare(
+        "sqrt(5 + 3 * sin(2)) / logb(2, 8)",
+        vec![
+            "5", "3", "2", "sin", "*", "+", "sqrt", "2", "8", "logb", "/",
+        ],
+    );
+    parse_and_compare(
+        "2 ^ (3 + abs(-4)) * max(5, min(6, 7))",
+        vec![
+            "2", "3", "4", "chs", "abs", "+", "^", "5", "6", "7", "min", "max", "*",
+        ],
+    );
+    parse_and_compare(
+        "sin(cos(2 * #PI / 3)) + 1 / (log(10) - 3.5)",
+        vec![
+            "2", "#PI", "*", "3", "/", "cos", "sin", "1", "10", "log", "3.5", "-", "/", "+",
+        ],
+    );
+    parse_and_compare(
+        "3 * (4 + 5) / (6 - abs(-7) * (8 + 9))",
+        vec![
+            "3", "4", "5", "+", "*", "6", "7", "chs", "abs", "8", "9", "+", "*", "-", "/",
+        ],
+    );
+    parse_and_compare(
+        "abs(-1 + 2) * ceil(sqrt(16)) - round(3.14159)",
+        vec![
+            "1", "chs", "2", "+", "abs", "16", "sqrt", "ceil", "*", "3.14159", "round", "-",
+        ],
+    );
+    parse_and_compare(
+        "sqrt(5*(((((1+0.2*(350/661.5)^2)^3.5-1)*(1-(6.875*10^-6)*25500)^-5.2656)+1)^0.286-1))",
+        vec![
+            "5", "1", "0.2", "350", "661.5", "/", "2", "^", "*", "+", "3.5", "^", "1", "-", "1",
+            "6.875", "10", "6", "chs", "^", "*", "25500", "*", "-", "5.2656", "chs", "^", "*", "1",
+            "+", "0.286", "^", "1", "-", "*", "sqrt",
+        ],
+    );
+
+    parse_and_compare("$A * $B + $C", vec!["$A", "$B", "*", "$C", "+"]);
+
+    parse_and_compare("$A + $B * $C", vec!["$A", "$B", "$C", "*", "+"]);
+
+    parse_and_compare("$A * ($B + $C)", vec!["$A", "$B", "$C", "+", "*"]);
+
+    parse_and_compare(
+        "34 * 5.3 ^ 2 + 0.9",
+        vec!["34", "5.3", "2", "^", "*", "0.9", "+"],
+    );
+
+    parse_and_compare(
+        "8e3 * ($B + 4.532 * -0.2) + $A",
+        vec!["8e3", "$B", "4.532", "0.2", "chs", "*", "+", "*", "$A", "+"],
+    );
 }

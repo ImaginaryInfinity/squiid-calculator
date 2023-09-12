ac4e1439093b30ecc08bba90644a48f96dabe3fa
TabulateJarl8 <tabulatejarl8@gmail.com>
2023-09-11 23:47:36 -0400

    start to fix the input buffer overflow error

--- src/app.rs
+++ src/app.rs
@@ -780,8 +780,25 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     };
 
     if app.input_mode == InputMode::Algebraic || app.input_mode == InputMode::Rpn {
+        let input_width = chunks[2].width as usize - 3; // Account for border characters
+
+        // Truncate the input if it's too long to fit in the available space
+        let truncated_input = if app.input.len() > input_width {
+            app.input[(app.input.len() - input_width)..].to_string()
+        } else {
+            app.input.clone()
+        };
+
+        // Calculate the cursor position based on the truncated input
+        let mut cursor_position_x = chunks[2].x + truncated_input.width() as u16 + 1;
+        if app.left_cursor_offset as usize > input_width {
+            app.left_cursor_offset = input_width as u16;
+        }
+
+        cursor_position_x -= app.left_cursor_offset;
+
         // THIS IS WHERE THE INPUT IS BEING ADDED TO THE PARAGRAPH DISPLAY
-        let input = Paragraph::new(app.input.as_ref())
+        let input = Paragraph::new(truncated_input.as_ref())
             .style(match app.input_mode {
                 _ if app.top_panel_state.currently_selecting() => Style::default(),
                 InputMode::None => Style::default(),
@@ -790,29 +807,8 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
             })
             .block(Block::default().borders(Borders::ALL).title(input_label));
         f.render_widget(input, chunks[2]);
-    }
-    match app.input_mode {
-        InputMode::None =>
-            // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
-            {}
 
-        InputMode::Algebraic | InputMode::Rpn if !app.top_panel_state.currently_selecting() => {
-            // Make the cursor visible and ask ratatui to put it at the specified coordinates after rendering
-
-            let mut cursor_position_x = chunks[2].x + app.input.width() as u16 + 1;
-            if app.left_cursor_offset as usize > app.input.width() {
-                app.left_cursor_offset = app.input.width() as u16;
-            }
-
-            cursor_position_x -= app.left_cursor_offset;
-            f.set_cursor(
-                // Put cursor past the end of the input text
-                cursor_position_x,
-                // Move one line down, from the border to the input line
-                chunks[2].y + 1,
-            )
-        }
-
-        _ => (),
+        // Set the cursor position
+        f.set_cursor(cursor_position_x, chunks[2].y + 1);
     }
 }

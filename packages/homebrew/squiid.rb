b2a6be957857a94601690f840d29412022ad80d8
TabulateJarl8 <tabulatejarl8@gmail.com>
2023-09-20 15:16:21 -0400

    homebrew code styling

--- packages/homebrew/squiid.rb
+++ packages/homebrew/squiid.rb
@@ -13,45 +13,42 @@ class Squiid < Formula
   end
 
   test do
-    require 'pty'
+    require "pty"
 
     PTY.spawn("squiid") do |r, w, pid|
-    begin
       # Wait for squiid to start
       sleep 1
-      
+
       # test that math works
       w.write "(10 - 2) * (3 + 5) / 4"
-      
+
       # send enter key
       w.write "\r"
-      
+
       # capture the stdout into output variable
-      output = ''
+      output = ""
       loop do
-      begin
         # strip off some color and style escape codes
-        output += r.read_nonblock(1024).gsub(/\e\[[0-9;]+[A-Za-z]/, '')
+        output += r.read_nonblock(1024).gsub(/\e\[[0-9;]+[A-Za-z]/, "")
       rescue IO::WaitReadable
-        break if IO.select([r], nil, nil, 2).nil?
+        break if r.wait_readable(2)
+
         retry
       rescue EOFError
         break
       end
-      end
-      
+
       # check that the calculator has done math correctly
       assert_match "(10-2)*(3+5)/4=16", output
-      
+
       # quit
       w.write "quit"
       w.write "\r"
-      
+
       # Wait for the TUI app to exit
       Process.wait(pid)
     rescue PTY::ChildExited
       # app exited
-    end
     end
   end
 end

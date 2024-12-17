36e581b1b5775c751e2a5facdd7ba2fc02552386
TabulateJarl8 <tabulatejarl8@gmail.com>
2024-12-17 17:23:13 -0500

    Move config stuff out of the backend and into the frontend

--- squiid-engine/src/engine.rs
+++ squiid-engine/src/engine.rs
@@ -5,7 +5,6 @@ use rust_decimal_macros::dec;
 
 use crate::{
     bucket::{build_exposed_constants, Bucket, BucketTypes, ConstantTypes},
-    config_handler,
     protocol::server_response::MessageAction,
     utils::{ID_REGEX, NUMERIC_REGEX},
 };
@@ -25,15 +24,12 @@ pub struct Engine {
     pub undo_state_pointer: u8,
     /// Previous answer
     pub previous_answer: Bucket,
-    /// Configuration struct
-    pub config: config_handler::Config,
 }
 
 /// Evaluation engine implementation
 impl Engine {
     /// Helper to construct a new engine object
     pub fn new() -> Engine {
-        config_handler::init_config();
         Engine {
             stack: Vec::new(),
             variables: HashMap::new(),
@@ -41,7 +37,6 @@ impl Engine {
             undo_variable_history: VecDeque::new(),
             undo_state_pointer: 0,
             previous_answer: Bucket::from(0),
-            config: config_handler::read_user_config().unwrap(),
         }
     }
 
